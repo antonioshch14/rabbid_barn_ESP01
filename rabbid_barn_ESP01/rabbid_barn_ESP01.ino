6838f081acc4a688a46c12624a2ca3f2481a51b6
Anton Belov <antonioshch14@gmail.com>
2020-05-05 10:03:01 +0300

    Add project files.

---
+++ rabbid_barn_ESP01/rabbid_barn_ESP01.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		rabbid_barn_ESP01.ino
+ Created:	2020-05-05 10:02:39
+ Author:	z003pr4u
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
